4916382422403d5c9c7401a08e2bf288298e6540
LUA Ting-Gan <banacorn@gmail.com>
2019-03-14 19:26:12 +0800

    refactoring

--- src/Type/TypeCheck.hs
+++ src/Type/TypeCheck.hs
@@ -26,7 +26,10 @@ import Control.Monad.Except
 --------------------------------------------------------------------------------
 -- | Error
 
-data TypeError = MissingProcDefn (Map ProcName Type)
+data TypeError
+  = MissingProcDefn (Map ProcName Type)
+  | VariableNotFound SName
+  | Others String
   deriving (Show)
 
 --------------------------------------------------------------------------------
@@ -61,29 +64,31 @@ checkAll = do
 
 type Cxt = FMap SName Type
 
-liftMaybe :: MonadError e m => e -> Maybe a -> m a
+liftMaybe :: TypeError -> Maybe a -> TCM a
 liftMaybe err = maybe (throwError err) return
 
 type TMonad a = Except ErrMsg a
 
-liftLookup :: (MonadError ErrMsg m, Eq a, Show a) => a -> FMap a b -> m b
-liftLookup x env =
-  liftMaybe ("variable " ++ show x ++ " not found") (lookup x env)
+lookupVar :: (Eq a, Show a) => FMap a Type -> a -> TCM Type
+lookupVar env x = case lookup x env of
+  Just v -> return v
+  Nothing -> throwError $ Others (show x)
 
-inferV :: MonadError ErrMsg m => Cxt -> Val -> m (Type, Cxt)
+inferV :: Cxt -> Val -> TCM (Type, Cxt)
 inferV _ (N (NR _)) =
-  throwError "StdOut/In in expression"
+  throwError $ Others "StdOut/In in expression"
 inferV cxt (N (ND c)) =
-  liftMaybe ("variable " ++ show c ++ " not found")
-    (lookup c cxt >>= \t ->
-     if unrestricted t then return (t, cxt)
-        else return (t, rmEntry c cxt ))
+  liftMaybe (Others $ "variable " ++ show c ++ " not found") $ do
+    t <- lookup c cxt
+    if unrestricted t
+      then return (t, cxt)
+      else return (t, rmEntry c cxt )
 inferV cxt (VI _) = return (tInt, cxt)
 inferV cxt (VB _) = return (tBool, cxt)
 inferV cxt (VT vs) =
   (TTuple *** id) <$> inferVs cxt vs
 -- inferV env (VL l) = liftMaybe "label not found" (lookup l env)
-inferV _ (VS _) = throwError "panic: not implemented yet"
+inferV _ (VS _) = throwError $ Others "panic: not implemented yet"
 -- inferV _ (VS _) = return TString
 
 inferVs cxt [v] = ((\t -> [t]) *** id) <$> inferV cxt v
@@ -92,8 +97,7 @@ inferVs cxt (v:vs) =
    inferVs cxt vs >>= \(ts, cxt'') ->
    return (t:ts, cxt'')
 
-inferE :: MonadError ErrMsg m =>
-          Cxt -> Expr -> m (Type, Cxt)
+inferE :: Cxt -> Expr -> TCM (Type, Cxt)
 inferE cxt (EV v) = inferV cxt v
 inferE cxt (EAdd e1 e2) =
   checkE cxt  e1 tInt >>= \cxt' ->
@@ -109,10 +113,10 @@ inferE cxt (EIf e0 e1 e2) =
   checkE cxt' e2 t    >>= \cxt''' ->
   if fMapEqBy eqType cxt'' cxt''' then
     return (t, cxt'')
-   else throwError "contexts fail to unify when checking If"
+   else throwError $ Others "contexts fail to unify when checking If"
 inferE cxt (ETup es) =
   (TTuple *** id) <$> inferEs cxt es
-inferE _ _ = throwError "panic: not implemented yet"
+inferE _ _ = throwError $ Others "panic: not implemented yet"
 
 inferEs cxt [e] = ((\t -> [t]) *** id) <$> inferE cxt e
 inferEs cxt (e:es) =
@@ -120,30 +124,29 @@ inferEs cxt (e:es) =
    inferEs cxt es >>= \(ts, cxt'') ->
    return (t:ts, cxt'')
 
-checkE :: MonadError ErrMsg m =>
-          Cxt -> Expr -> Type -> m Cxt
+checkE :: Cxt -> Expr -> Type -> TCM Cxt
 checkE env e t =
   inferE env e >>= \(t', cxt) ->
   tcheck t t' >> return cxt
 
-tcheck :: MonadError ErrMsg m => Type -> Type -> m ()
+tcheck :: Type -> Type -> TCM ()
 tcheck t1 t2
    | eqType t1 t2 = return ()
-   | otherwise = throwError ("expected: " ++ show t1 ++
+   | otherwise = throwError $ Others ("expected: " ++ show t1 ++
                              ", found " ++ show t2)
 
-checkCh :: MonadError ErrMsg m => Cxt -> PN RName -> Type -> m ()
-checkCh senv c t =
-  liftLookup c senv >>= \t' ->
-  if t == t' then return ()
-    else throwError "type mismatch"
+checkCh :: Cxt -> PN RName -> Type -> TCM ()
+checkCh senv c t = do
+  t' <- lookupVar senv c
+  if t == t'
+    then return ()
+    else throwError $ Others "type mismatch"
 
 allClosed :: Cxt -> Bool
 allClosed = all (TEnd ==) . map snd
         -- = const True
 
-checkPi :: MonadError ErrMsg m =>
-   Cxt -> Pi -> m (Cxt, [PN RName])
+checkPi :: Cxt -> Pi -> TCM (Cxt, [PN RName])
 
 checkPi cxt End = return (cxt, [])
 
@@ -156,60 +159,60 @@ checkPi cxt (Send (NR StdOut) e p) =
   inferE  cxt e >>= \(_, cxt') ->
   checkPi cxt' p
 checkPi cxt (Send (NR _) e p) =
-  throwError "cannot send to this channel"
+  throwError $ Others "cannot send to this channel"
 checkPi cxt (Send (NG _) e p) =
-  throwError "generated name appears in type checking. a bug?"
+  throwError $ Others "generated name appears in type checking. a bug?"
 checkPi cxt (Send (ND c) e p) =
   lookupChType c cxt >>=  -- c removed if linear
   matchSend (checkPiSend (c,e,p)) (checkPiSel (c,e,p))
 
 checkPi _ (Recv (NR _) _) =
-  throwError "not knowing what to do yet"
+  throwError $ Others "not knowing what to do yet"
 checkPi _ (Recv (NG _) _) =
-  throwError "generated name appears in type checking. a bug?"
+  throwError $ Others "generated name appears in type checking. a bug?"
 checkPi cxt (Recv (ND c) ps) =
   lookupChType c cxt >>= -- c removed if linear
   matchRecv (checkPiRecv (c,ps)) (checkPiChoi (c,ps))
 
-checkPi cxt (Repl p) = -- throwError "panic: not implemented yet"
+checkPi cxt (Repl p) = -- throwError $ Others "panic: not implemented yet"
   checkPi cxt p >>= \(cxt', l) ->
   if null l then return (cxt', l)
-     else throwError ("linear variable used in repetition")
+     else throwError $ Others ("linear variable used in repetition")
 
 checkPi _ (Nu _ Nothing _) =
-  throwError "needing a type for new channels"
+  throwError $ Others "needing a type for new channels"
 checkPi cxt (Nu x (Just t) p) =
   checkPi ((Pos x, t):(Neg x, dual t):cxt) p >>= \(cxt', l) ->
   cxt' `sMinus` [Pos x, Neg x] >>= \cxt'' ->
   return (cxt'', l `setminus` [Pos x, Neg x])
 
-checkPiSend :: MonadError ErrMsg m =>
+checkPiSend ::
   (SName, Expr, Pi) ->
-  Type -> Type -> Bool -> Cxt -> m (Cxt, [SName])
+  Type -> Type -> Bool -> Cxt -> TCM (Cxt, [SName])
 checkPiSend (c,e,p) s t un cxt =
   checkE cxt e s >>= \cxt' ->
   addCxt (c,t) cxt' >>= \cxt'' ->
   (id *** addLin un c) <$>
          checkPi cxt'' p
 
-checkPiSel :: MonadError ErrMsg m =>
+checkPiSel ::
   (SName, Expr, Pi) ->
-  [(Label, Type)] -> Bool -> Cxt -> m (Cxt, [SName])
-checkPiSel (c,e,p) ts un cxt =
-  isLabel e >>= \l ->
-  liftLookup l ts >>= \s ->
-  addCxt (c,s) cxt >>= \cxt' ->
+  [(Label, Type)] -> Bool -> Cxt -> TCM (Cxt, [SName])
+checkPiSel (c,e,p) ts un cxt = do
+  l <- isLabel e
+  s <- lookupVar ts l
+  cxt' <- addCxt (c,s) cxt
   (id *** addLin un c) <$> checkPi cxt' p
 
-checkPiRecv :: MonadError ErrMsg m =>
+checkPiRecv ::
   (SName, [Clause]) ->
-  Type -> Type -> Bool -> Cxt -> m (Cxt, [SName])
+  Type -> Type -> Bool -> Cxt -> TCM (Cxt, [SName])
 checkPiRecv (c, ps) s t un cxt =
   mapM (checkRecvClause c s t un cxt) ps >>= eqAll
 
-checkRecvClause :: MonadError ErrMsg m =>
+checkRecvClause ::
   SName -> Type -> Type -> Bool -> Cxt ->
-  Clause -> m (Cxt, [SName])
+  Clause -> TCM (Cxt, [SName])
 checkRecvClause c s t un cxt (Clause ptn p) =
   matchPtn s ptn cxt >>= \cxt' ->
   addCxt (c,t) cxt' >>= \cxt'' ->
@@ -219,9 +222,9 @@ checkRecvClause c s t un cxt (Clause ptn p) =
           in if un then l' else (nubcons c l'))
  where xs = ptnVars ptn
 
-checkPiChoi :: MonadError ErrMsg m =>
+checkPiChoi ::
   (SName, [Clause]) ->
-  [(Label, Type)] -> Bool -> Cxt -> m (Cxt, [SName])
+  [(Label, Type)] -> Bool -> Cxt -> TCM (Cxt, [SName])
 checkPiChoi (c, ps) ts un cxt =
    pairChoices ts ps >>= \xss ->
    mapM (\(s,p) -> addCxt (c,s) cxt >>= \cxt' ->
@@ -231,26 +234,26 @@ checkPiChoi (c, ps) ts un cxt =
 addLin True  _ xs  = xs
 addLin False c xs = nubcons c xs
 
-isLabel :: MonadError ErrMsg m => Expr -> m Label
+isLabel ::  Expr -> TCM Label
 isLabel (EV (VL l)) = return l
-isLabel _ = throwError "must be a label"
+isLabel _ = throwError $ Others "must be a label"
 
 -- generic dispatcher
 
-matchSend :: MonadError ErrMsg m =>
-   (Type -> Type -> Bool -> Cxt -> m a) ->
-   ([(Label, Type)] -> Bool -> Cxt -> m a) -> (Type, Bool, Cxt) -> m a
+matchSend ::
+   (Type -> Type -> Bool -> Cxt -> TCM a) ->
+   ([(Label, Type)] -> Bool -> Cxt -> TCM a) -> (Type, Bool, Cxt) -> TCM a
 matchSend fs _  (TSend s1 s2, b, cxt) = fs s1 s2 b cxt
 matchSend _  fl (TSele ts,    b, cxt) = fl ts b cxt
 matchSend _  _  t =
-  throwError ("TSend expected, got " ++ show t)
+  throwError $ Others ("TSend expected, got " ++ show t)
 
-matchRecv :: MonadError ErrMsg m =>
-  (Type -> Type -> Bool -> Cxt -> m a) ->
-  ([(Label, Type)] -> Bool -> Cxt -> m a) -> (Type, Bool, Cxt) -> m a
+matchRecv ::
+  (Type -> Type -> Bool -> Cxt -> TCM a) ->
+  ([(Label, Type)] -> Bool -> Cxt -> TCM a) -> (Type, Bool, Cxt) -> TCM a
 matchRecv fs _  (TRecv t s, b, cxt) = fs t s b cxt
 matchRecv _  fl (TChoi ts,  b, cxt) = fl ts b cxt
-matchRecv _  _  _ = throwError "TRecv expected"
+matchRecv _  _  _ = throwError $ Others "TRecv expected"
 
 -- pattern related stuffs
 
@@ -259,8 +262,7 @@ ptnVars (PN x) = [Pos x]  -- right?
 ptnVars (PT xs) = concat (map ptnVars xs)
 ptnVars (PL _) = []
 
-matchPtn :: MonadError ErrMsg m =>
-   Type -> Ptrn -> Cxt -> m Cxt
+matchPtn :: Type -> Ptrn -> Cxt -> TCM Cxt
 matchPtn TEnd (PN x) cxt = addUni (Pos x,TEnd) cxt
 matchPtn (TBase t) (PN x) cxt = addUni (Pos x,TBase t) cxt
 matchPtn (TTuple ts) (PN x) cxt = addUni (Pos x, TTuple ts) cxt
@@ -272,17 +274,17 @@ matchPtn (TChoi ts) (PN x) cxt = addUni (Pos x, TChoi ts) cxt
 matchPtn (TUn t) (PN x) cxt = addUni (Pos x, TUn t) cxt
 matchPtn (TUn (TTuple ts)) (PT xs) cxt =
   matchPtns (map TUn ts) xs cxt
-matchPtn _ _ _ = throwError "pattern mismatch"
+matchPtn _ _ _ = throwError $ Others "pattern mismatch"
 
 matchPtns []     []     cxt = return cxt
 matchPtns (t:ts) (x:xs) cxt =
   matchPtn t x cxt >>=
   matchPtns ts xs
-matchPtns _ _ _ = throwError "tuple length mismatch"
+matchPtns _ _ _ = throwError $ Others "tuple length mismatch"
 
 -- env related utilities
 
-sMinus :: MonadError ErrMsg m => Cxt -> [SName] -> m Cxt
+sMinus :: Cxt -> [SName] -> TCM Cxt
 cxt `sMinus` []     = return cxt
 cxt `sMinus` (x:xs) =
   cxt `sMinus` xs >>= \cxt' ->
@@ -290,53 +292,48 @@ cxt `sMinus` (x:xs) =
     (return cxt')
     (\t -> if unrestricted t then
                return (rmEntry x cxt)
-           else throwError ("channel " ++ show x ++ " not used up."))
+           else throwError $ Others ("channel " ++ show x ++ " not used up."))
 
-lookupChType :: MonadError ErrMsg m =>
-  SName -> Cxt -> m (Type, Bool, Cxt)
-lookupChType x cxt =
-  liftLookup x cxt >>= \t ->
+lookupChType :: SName -> Cxt -> TCM (Type, Bool, Cxt)
+lookupChType x cxt = do
+  t <- lookupVar cxt x
   stripUnres (unfoldT t) & \(t', unrest) ->
-  if unrest then return (t', unrest, cxt)
-      else return (t', unrest, rmEntry x cxt)
+    if unrest then return (t', unrest, cxt)
+        else return (t', unrest, rmEntry x cxt)
 
-addCxt :: MonadError ErrMsg m =>
-      (SName, Type) -> Cxt -> m Cxt
+addCxt :: (SName, Type) -> Cxt -> TCM Cxt
 addCxt (c,t) cxt =
   lookup c cxt &
    maybe (return ((c,t) : cxt))
      (\t' -> if eqType t t' then return cxt
-              else throwError ("types of channels in env mismatch: " ++ show t ++ " and " ++ show t'))
+              else throwError $ Others ("types of channels in env mismatch: " ++ show t ++ " and " ++ show t'))
 
-addUni :: MonadError ErrMsg m =>
-      (SName, Type) -> Cxt -> m Cxt
+addUni :: (SName, Type) -> Cxt -> TCM Cxt
 addUni (x,t) cxt =
   lookup x cxt &
    maybe (return ((x,t):cxt))
-     (\_ -> throwError (show x ++ " exists in context"))
+     (\_ -> throwError $ Others (show x ++ " exists in context"))
 
-eqAll :: MonadError ErrMsg m =>
-   [(Cxt, [SName])] -> m (Cxt, [SName])
+eqAll :: [(Cxt, [SName])] -> TCM (Cxt, [SName])
 eqAll [x] = return x
 eqAll ((s1,l1):(s2,l2):xs) =
   if (fMapEqBy eqType s1 s2) && (l1 `setEq` l2)
     then eqAll ((s2,l2):xs)
-    else throwError "branches cannot be unified"
+    else throwError $ Others "branches cannot be unified"
 
-pairChoices :: MonadError ErrMsg m =>
-  [(Label, Type)] -> [Clause] -> m [(Type, Pi)]
+pairChoices :: [(Label, Type)] -> [Clause] -> TCM [(Type, Pi)]
 pairChoices [] [] = return []
 pairChoices _ [] =
-  throwError "choices not fully covered"
-pairChoices ts (Clause (PL l) p : ps) =
-  liftLookup l ts >>= \s ->
+  throwError $ Others "choices not fully covered"
+pairChoices ts (Clause (PL l) p : ps) = do
+  s <- lookupVar ts l
   ((s,p):) <$> pairChoices (rmEntry l ts) ps
 pairChoices _ (Clause _ _ : _) =
-  throwError "not a label"
+  throwError $ Others "not a label"
 
 {-
-splitSEnv :: MonadError ErrMsg m =>
-  [PN RName] -> [PN RName] -> SEnv -> m (SEnv, SEnv)
+splitSEnv ::
+  [PN RName] -> [PN RName] -> SEnv -> TCM (SEnv, SEnv)
 splitSEnv _ _ [] = return ([],[])
 splitSEnv fl fr ((x,t):xs)
    | bl && br  = throwError ("channel " ++ show x ++ " not linear")
@@ -380,8 +377,8 @@ pn = PN . pack
 cp = Pos . pack
 cn = Neg . pack
 
-test0 :: Either String (Cxt, [SName])
-test0 = runExcept $ checkPi cxt p
+test0 :: Either TypeError (Cxt, [SName])
+test0 = runTCM (checkPi cxt p) Map.empty
   where cxt = [(cp "c", tsend TInt (tsend TBool TEnd)),
                (cp "d", TSend (TTuple [tInt, tsend TBool TEnd]) TEnd)]
         p = Send (cP "c") (eI 3) $
@@ -389,13 +386,13 @@ test0 = runExcept $ checkPi cxt p
         {- p = c[3].d[4,c].0
         -}
 
-test1 :: Either String (Cxt, [SName])
-test1 = runExcept $ checkPi cxt p
+test1 :: Either TypeError (Cxt, [SName])
+test1 = runTCM (checkPi cxt p) Map.empty
   where cxt = [(cn "c", trecv TInt (trecv TBool TEnd))]
         p = recv (cN "c") (pn "x")  $ recv (cN "c") (pn "y")  End
 
-test2 :: Either String (Cxt, [SName])
-test2 = runExcept $ checkPi [] p
+test2 :: Either TypeError (Cxt, [SName])
+test2 = runTCM (checkPi cxt p) Map.empty
   where p = nu "c" t (Par p1 p2)
         p1 = Send (cP "c") (eI 3) $ Send (cP "c") (eB False) End
         p2 = recv (cN "c") (pn "x") $ recv (cN "c") (pn "y") End
@@ -407,8 +404,8 @@ test2 = runExcept $ checkPi [] p
             t  = !Int.!Bool.0
         -}
 
-test3 :: Either String (Cxt, [SName])
-test3 = runExcept $ checkPi cxt0 p2
+test3 :: Either TypeError (Cxt, [SName])
+test3 = runTCM (checkPi cxt0 p0) Map.empty
   where
     t0 :: Type
     t0 = tsele [("NEG", tsend TInt  $ trecv TInt  $ TEnd),
@@ -444,15 +441,15 @@ test3 = runExcept $ checkPi cxt0 p2
     p2 :: Pi
     p2 = nu "c" t1 (nu "d" t0 p0 `Par` p1)
 
-test4 :: Either String (Cxt, [SName])
-test4 = runExcept $ checkPi cxt p
+test4 :: Either TypeError (Cxt, [SName])
+test4 = runTCM (checkPi cxt p) Map.empty
   where cxt = [(cp "c", TMu $ TUn $ trecv TInt $ TVar 0)]
         p = Repl (recv (cP "c") (pn "x") End)
         -- p = *(c(x).0)
         -- {c: mu(X)un(?Int.X)}
 
-test5 :: Either String (Cxt, [SName])
-test5 = runExcept $ checkPi cxt p
+test5 :: Either TypeError (Cxt, [SName])
+test5 = runTCM (checkPi cxt p) Map.empty
   where t = tsend TInt $ trecv TBool TEnd
         -- {c : mu(X)(un(?(!Int.?Bool.0).X))}
         -- p = *(c(d). d[3].d(x).0)
